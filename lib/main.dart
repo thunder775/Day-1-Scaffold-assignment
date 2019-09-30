268fb13e74a6b4690a8412dd0675cda479c72907
rahul masih <rahulmasih61@gmail.com>
2019-09-30 16:23:10 +0530

    solved

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,25 @@ void main() {
   // Link to icon: https://mclarencollege.in/images/icon.png
   // Link to center image: https://mclarencollege.in/images/9fc76a72-5b06-432f-b92e-d41d8ad5629f.jpg
 
-  String msg = 'Hello World';
+  String msg = 'I Love Flutter';
   Text textWidget = Text(msg);
   Center center = Center(child: textWidget);
-  MaterialApp myApp = MaterialApp(home: center);
+  AppBar bar1 = AppBar(
+    leading:
+        Image(image: NetworkImage('https://mclarencollege.in/images/icon.png')),
+    title: center,
+    backgroundColor: Colors.lightBlue,
+  );
+
+  Scaffold coolSca = Scaffold(
+      backgroundColor: Colors.white,
+      appBar: bar1,
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              'https://mclarencollege.in/images/9fc76a72-5b06-432f-b92e-d41d8ad5629f.jpg'),
+        ),
+      ));
+  MaterialApp myApp = MaterialApp(home: coolSca);
   runApp(myApp);
 }
